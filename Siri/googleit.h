e0e792a79835a23930fbd285bc57d08bbdbc2c63
Synnove <olsdattersynnove@gmail.com>
2023-11-29 18:16:50 -0500

    Added the 'here's what I found' thing

--- Siri/googleit.h
+++ Siri/googleit.h
@@ -8,13 +8,22 @@
 #define _GOOGLING
 #endif
 #ifdef _WIN32
-//TODO: ADD CODE AFTER COMPLETED USING ShellExecute(NULL, "open", "https://URL", NULL, NULL, SW_SHOWNORMAL) instead
+#include <shellapi.h>
+void google_it(const char* prompt, int pl) {// pl -> prompt_len
+    char url[500] = "https://www.google.com/search?q=";
+    for (int i = 0; i < pl; i++) {
+        if (prompt[i]==32) url[(int)strlen(url)]='+' else url[(int)strlen(url)]=prompt[i];
+    }
+    printf("\nHere's what I found.\7\n");
+    ShellExecute(NULL, "open", url, NULL, NULL, SW_SHOWNORMAL);
+}
 #else
 void google_it(const char* prompt, int pl) {// pl -> prompt_len
     char url[500] = "open https://www.google.com/search?q=";
     for (int i = 0; i < pl; i++) {
         if (prompt[i]==32) url[(int)strlen(url)]='+' else url[(int)strlen(url)]=prompt[i];// USUALLY doesn't crash!
     }
+    printf("\nHere's what I found.\7\n");
     system(url);// runs the command 'open' because i included it in the url variable
 }
 #endif
